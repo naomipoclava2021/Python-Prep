4acfa300db3b873b0431e5b7bfd30739c41fd6b4
naomipoclava2021 <milagrito1dediciembre@gmail.com>
2022-03-02 14:56:13 -0300

    clase2.py

---
+++ 02 - Variables y Tipos de Datos/.ipynb_checkpoints/clase2.py
@@ -0,0 +1,84 @@
+#1) Crear una variable que contenga un elemento del conjunto de números enteros y luego imprimir por pantalla
+n = 12;
+print(n);
+
+#2) Imprimir el tipo de dato de la constante 8.5
+print(type(8.5));
+
+#3) Imprimir el tipo de dato de la variable creada en el punto 1
+print(type(n));
+
+#4) Crear una variable que contenga tu nombre
+mi_nombre = 'Naomi';
+
+#5) Crear una variable que contenga un número complejo
+numero_complex = 1 + 1J;
+
+#6) Mostrar el tipo de dato de la variable crada en el punto 5
+print(type(mi_nombre));
+
+#7) Crear una variable que contenga el valor del número Pi redondeado a 4 decimales
+
+pi = 3.1416;
+
+#8) Crear una variable que contenga el valor 'True' y otra que contenga el valor True. ¿Se trata de lo mismo?
+
+valor1 = 'True';
+valor2 = True;
+
+#9) Imprimir el tipo de dato correspondientes a las variables creadas en el punto 9
+print('la variable valor1 es de tipo: ', type(valor1), ' y la variable valor 2 es de tipo: ', type(valor2));
+
+#10) Asignar a una variable, la suma de un número entero y otro decimal
+suma = 12 + 1.21
+
+#11) Realizar una operación de suma de números complejos
+a = 1 + 1j
+b = 2 + 2j
+print(a+b)
+
+#12) Realizar una operación de suma de un número real y otro complejo
+s = a + 12;
+print(s);
+
+#13) Realizar una operación de multiplicación
+num1 = 9;
+num2 = 2;
+m = num1 * num2
+print(m)
+
+#14) Mostrar el resultado de elevar 2 a la octava potencia
+print(2**8);
+
+#15) Obtener el cociente de la división de 27 entre 4 en una variable y luego mostrarla
+cociente = 27 / 4;
+print(cociente)
+
+#16) De la división anterior solamente mostrar la parte entera
+print(27// 4);
+
+#17) De la división de 27 entre 4 mostrar solamente el resto
+print(27 % 4);
+
+#18) Utilizando como operandos el número 4 y los resultados obtenidos en los puntos 16 y 17. Obtener 27 como resultado
+print(4 * 6 + 3);
+
+#19) Utilizar el operador "+" en una operación donde intervengan solo variables alfanuméricas
+
+
+#20) Evaluar si "2" es igual a 2. ¿Por qué ocurre eso?
+print('2' == 2);
+
+#21) Utilizar las funciones de cambio de tipo de dato, para que la validación del punto 20 resulte verdadera
+print(int('2') == 2);
+
+#22) ¿Por qué arroja error el siguiente cambio de tipo de datos? a = float('3,8')
+print(int('3,8'))
+
+#23) Crear una variable con el valor 3, y utilizar el operador '-=' para modificar su contenido
+
+#24) Realizar la operacion 1 << 2 ¿Por qué da ese resultado? ¿Qué es el sistema de numeración binario?
+
+#25) Realizar la operación 2 + '2' ¿Por qué no está permitido? ¿Si los dos operandos serían del mismo tipo, siempre arrojaría el mismo resultado?
+
+#26) Realizar una operación válida entre valores de tipo entero y string
